ba545e46c352107162f8d4bc1dfe83fab643b7ce
Ahmed Abd-El Fattah <52181444+AhmedAbd-ElFattah@users.noreply.github.com>
2023-09-28 17:36:26 +0300

    Delete STM32-Custom-Bootloader/Debug directory

--- STM32-Custom-Bootloader/Debug/STM32-Drivers/HAL/TFTLCD/TFTTestImage.d
+++
@@ -1,6 +0,0 @@
-STM32-Drivers/HAL/TFTLCD/TFTTestImage.o: \
- ../STM32-Drivers/HAL/TFTLCD/TFTTestImage.c \
- ../STM32-Drivers/HAL/TFTLCD/TFTTestImage.h \
- /home/ahmed/Desktop/Github/STM32-Custom-Bootloader/STM32-Custom-Bootloader/STM32-Drivers/LIB/STD_TYPES.h
-../STM32-Drivers/HAL/TFTLCD/TFTTestImage.h:
-/home/ahmed/Desktop/Github/STM32-Custom-Bootloader/STM32-Custom-Bootloader/STM32-Drivers/LIB/STD_TYPES.h:
